1c3d472bf88fecf69eb2c3d6530a83d5e36cc0aa
soroush4208 <soroush.r80@gmail.com>
2024-06-02 17:53:48 +0330

    feat : create RegisterPage

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
+import { ROUTES } from "../../router/path";
+
+function Register() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col justify-center items-center align-middle h-[100vh]">
+      <div className="glitch-wrapper cursor-pointer">
+        <button
+          onClick={() => navigate(ROUTES.SignIn)}
+          className="glitch text-3xl sm:text-[80px]"
+          data-glitch="Go to Weather app"
+        >
+          Go to Weather app
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
