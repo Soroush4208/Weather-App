6201694522a552af4da87cc0806612373648a97e
soroush4208 <soroush.r80@gmail.com>
2024-06-03 01:56:19 +0330

    feat : create sign up page

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,5 +1,61 @@
-const SignInPage = () => {
-  return <div>SignInPage</div>;
+import { Button, TextField } from "@mui/material";
+import GOOGLE from "../../assets/svgs/google.svg";
+import GITHUB from "../../assets/svgs/github.svg";
+import LINKEDIN from "../../assets/svgs/linkedin.svg";
+import TELEGRAM from "../../assets/svgs/telegram.svg";
+
+const SignIn = () => {
+  return (
+    <div className="flex flex-col  justify-center h-screen items-start gap-5 w-1/3">
+      <div className="w-full flex justify-center">
+        <h1 className="font-kalam font-bold text-3xl">Sing up</h1>
+      </div>
+      <div className="flex flex-col gap-5 w-full">
+        <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" variant="outlined" />
+      </div>
+
+      <Button
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: 1,
+          borderRadius: 7,
+          py: 2,
+        }}
+        variant="contained"
+      >
+        Sign up
+      </Button>
+      <div className="flex flex-col gap-4 justify-center items-center w-full font-kalam">
+        <p className="font-bold text-xl">Or Sign up witch social platform</p>
+        <div className="flex gap-2 ">
+          <img
+            src={GOOGLE}
+            className="p-2 border border-black rounded-full w-12 cursor-pointer hover:bg-gray-300"
+            alt="GOOGLE icon"
+          />
+          <img
+            src={GITHUB}
+            className="p-2 border border-black rounded-full w-12 cursor-pointer hover:bg-gray-300"
+            alt="GITHUB icon"
+          />
+          <img
+            src={LINKEDIN}
+            className="p-2 border border-black rounded-full w-12 cursor-pointer hover:bg-gray-300"
+            alt="LINKEDIN icon"
+          />
+          <img
+            src={TELEGRAM}
+            className="p-2 border border-black rounded-full w-12 cursor-pointer hover:bg-gray-300"
+            alt="TELEGRAM icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignInPage;
+export default SignIn;
